59b1c5143566f53e9bd81e5611c2ede5eca545e5
machintoshssd <dev.saeculumsolutions@gmail.com>
2021-03-03 11:59:19 +0530

    Build issue in github case

--- app/src/main/java/com/example/androiddevchallenge/ui/view/Toolbar.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/view/Toolbar.kt
@@ -1,17 +1,16 @@
 package com.example.androiddevchallenge.ui.view
 
 
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androiddevchallenge.R
-import com.example.androiddevchallenge.data.Puppy
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 @Preview
@@ -29,36 +28,6 @@ fun Toolbar() {
                 }
             },
             title = { Text(text = stringResource(id = R.string.app_name)) }
-        )
-    }
-}
-
-@Composable
-fun ToolbarForDetail(
-    puppy: Puppy,
-    navigateBack: () -> Unit
-) {
-    MyTheme {
-        TopAppBar(
-            navigationIcon = {
-                IconButton(onClick = navigateBack) {
-                    Icon(
-                        Icons.Filled.ArrowBack,
-                        contentDescription = null,
-                        tint = Color.White
-                    )
-                }
-            },
-            title = { Text(text = puppy.name) },
-            actions = {
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(
-                        Icons.Filled.FavoriteBorder,
-                        contentDescription = "Add to favorites",
-                        tint = MaterialTheme.colors.onPrimary
-                    )
-                }
-            }
         )
     }
 }
---
+++ app/src/main/java/com/example/androiddevchallenge/ui/view/ToolbarForDetail.kt
@@ -0,0 +1,40 @@
+package com.example.androiddevchallenge.ui.view
+
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.example.androiddevchallenge.data.Puppy
+import com.example.androiddevchallenge.ui.theme.MyTheme
+
+@Composable
+fun ToolbarForDetail(
+    puppy: Puppy,
+    navigateBack: () -> Unit
+) {
+    MyTheme {
+        TopAppBar(
+            navigationIcon = {
+                IconButton(onClick = navigateBack) {
+                    Icon(
+                        Icons.Filled.ArrowBack,
+                        contentDescription = null,
+                        tint = Color.White
+                    )
+                }
+            },
+            title = { Text(text = puppy.name) },
+            actions = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        Icons.Filled.FavoriteBorder,
+                        contentDescription = "Add to favorites",
+                        tint = MaterialTheme.colors.onPrimary
+                    )
+                }
+            }
+        )
+    }
+}
